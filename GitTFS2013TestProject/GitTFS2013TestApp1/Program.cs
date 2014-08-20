2c1a72b26dfa1187c28724783692f3ebd0db709c
secondhandbuddah <secondhand.buddah@gmail.com>
2014-08-20 09:08:27 +0200

    First Update

--- GitTFS2013TestProject/GitTFS2013TestApp1/Program.cs
+++ GitTFS2013TestProject/GitTFS2013TestApp1/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace GitTFS2013TestApp1
 {
+    /// <summary>
+    /// This is a sample test project
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
